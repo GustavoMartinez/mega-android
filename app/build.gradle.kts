53848c3c08f9c38617661f8d8ec9e3c8ef426f3b
Gustavo <gustavo.martinez@hotmail.cl>
2025-09-24 20:05:03 -0300

    ExoPlayer added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,13 @@ dependencies {
 
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+
+    implementation(libs.androidx.media3.exoplayer.hls)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.exoplayer.smoothstreaming)
+
+    implementation(libs.androidx.media3.cast)
 }
