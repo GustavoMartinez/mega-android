554aa9ac4d696974680b4ca5b30a19a87e7261b3
Gustavo <gustavo.martinez@hotmail.cl>
2025-09-26 12:42:37 -0300

    gitignore modified

--- app/src/main/java/com/app/megaandroid/MegaAndroid.kt
+++ app/src/main/java/com/app/megaandroid/MegaAndroid.kt
@@ -1,4 +1,8 @@
 package com.app.megaandroid
 
-class MegaAndroid {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MegaAndroid : Application() {
 }
